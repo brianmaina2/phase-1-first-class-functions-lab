19bf6c834426c7a7108b21ae41dade2f428dd72e
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-25 23:19:07 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(drivers){
-    return drivers.slice(0,2)
-}
-function returnLastTwoDrivers(drivers) {
-    return drivers.slice(-2)
-}
-let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
-function createFareMultiplier(n){
-    return(function(x){
-        return n*x
-    })
-}
-const fareDoubler = createFareMultiplier(2)
-const fareTripler = createFareMultiplier(3)
-function selectingDrivers(arrayOfDrivers,callback){
-    return callback(arrayOfDrivers)
-}
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0,2)
+  }
+  const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2)
+  }
+  let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+  const createFareMultiplier = function(x) {
+    return function(fare) {
+      return fare*x
+    }
+  }
+  const fareDoubler = createFareMultiplier(2)
+  const fareTripler = createFareMultiplier(3)
+  const selectDifferentDrivers = function(arr, x) {
+    return x(arr)
+  }
